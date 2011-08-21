2c6b55b314eb1a35878e2f4cccebe4981b488cbc
wxm <weekly123@gmail.com>
2011-08-21 13:54:04 +0800

    init

---
+++ test/utest.cpp
@@ -0,0 +1,17 @@
+/*
+ * utest.cpp
+ *
+ *  Created on: 2011-8-21
+ *      Author: wxm
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char * argv[])
+{
+	cout<<"hello world!"<<endl;
+	return 0;
+}
+
+
